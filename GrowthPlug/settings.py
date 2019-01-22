22bf95319b9e5c1eb2e5643ed078c973a8c04bd0
shreyanshs7 <shreyanshss7@gmail.com>
2019-01-22 11:01:35 +0530

    Added heroku deployment files

--- GrowthPlug/settings.py
+++ GrowthPlug/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn GrowthPlug.wsgi --log-file -
